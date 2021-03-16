e82ad9a195d57858f02633d70583fe9a189c5c6c
windhamdavid <david@davidawindham.com>
2021-03-16 06:03:04 -0400

    favicon

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title><?php global $page, $paged; wp_title( '|', true, 'right' ); bloginfo( 'name' ); $site_description = get_bloginfo( 'description', 'display' ); if ( $site_description && ( is_home() || is_front_page() ) ) echo " | $site_description"; if ( $paged >= 2 || $page >= 2 ) echo ' | ' . sprintf( __( 'Page %s', '_ph' ), max( $paged, $page ) ); ?></title>
 <!--[if lt IE 9]><script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script><![endif]-->
+<link rel="shortcut icon" type="image/x-icon" href="<?php echo home_url();?>/favicon.ico" />
 <?php wp_head(); ?>
 </head>
 <body>
